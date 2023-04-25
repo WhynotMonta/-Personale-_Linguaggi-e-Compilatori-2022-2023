a06278261d94a24d610565ffd84d80df53acea94
LorenzoCodeluppi <26163565+LorenzoCodeluppi@users.noreply.github.com>
2023-04-25 12:35:18 +0200

    add exit blocks check

--- Tutorial-03/Assignement 2/lib/LoopWalk.cpp
+++ Tutorial-03/Assignement 2/lib/LoopWalk.cpp
@@ -20,35 +20,39 @@ class LoopWalkPass final : public LoopPass {
   }
 
   virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
+    std::map<Instruction*, bool> invariantMap;
+    SmallVector<BasicBlock*> exitBlocks;
+    
     DominatorTree *DT = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
     BasicBlock *preheader = L->getLoopPreheader();
-    std::map<Instruction*, bool> invariantMap;
     
-    for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end(); ++BI) {
-      BasicBlock *actualBB = *BI;
-      for (Instruction &instr : *actualBB) {
+    for (auto &BI : L->getBlocks()) {
+      for (Instruction &instr : *BI) {
         bool isInvariant = true;
         if(instr.isBinaryOp() ) { 
           outs()<<"Istruzione:"<<instr<<'\n';
           for(auto *Iter=instr.op_begin(); Iter!=instr.op_end();++Iter) {
             Value *operand=*Iter;
             outs()<<"Istruzione che definisce l'operando:"<<*operand<<'\n';
-            check(operand, L, isInvariant);
+            check(operand, L, isInvariant, invariantMap);
           }
           outs() << isInvariant <<"\n";
         }
       }
     }
+
+    L->getExitBlocks(exitBlocks);
+
     return false;
   }
 
-  void check(Value* operand, Loop *L, bool &isInvariant) {
-    if (dyn_cast<ConstantInt>(operand)){              
+  void check(Value* operand, Loop *L, bool &isInvariant, std::map<Instruction*, bool> invariantMap) {
+    if (dyn_cast<ConstantInt>(operand))              
       return;
     Instruction* reachingDef = dyn_cast<Instruction>(operand);
     if(reachingDef) {
       BasicBlock* bb = reachingDef->getParent();
-      if(!L->contains(bb))
+      if(L->contains(bb) && !invariantMap[reachingDef] )
         isInvariant = false;
     }
   }
