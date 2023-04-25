d3551cfbb6ab829915f9178a2c3d8ab78d71bd5d
LorenzoCodeluppi <26163565+LorenzoCodeluppi@users.noreply.github.com>
2023-04-25 16:33:44 +0200

    assignement 2

--- Tutorial-03/Assignement 2/lib/LoopWalk.cpp
+++ Tutorial-03/Assignement 2/lib/LoopWalk.cpp
@@ -1,7 +1,9 @@
 #include <llvm/Analysis/LoopPass.h>
 #include <llvm/Analysis/ValueTracking.h>
 #include <llvm/IR/Dominators.h>
+#include <iostream>
 #include <map>
+#include <vector>
 
 using namespace llvm;
 
@@ -22,26 +24,72 @@ class LoopWalkPass final : public LoopPass {
   virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
     std::map<Instruction*, bool> invariantMap;
     SmallVector<BasicBlock*> exitBlocks;
+    std::vector<Instruction*> CMcandidates;
+    std::vector<Instruction*> removable;
     
     DominatorTree *DT = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
     BasicBlock *preheader = L->getLoopPreheader();
     
+    // cerchiamo le istruzioni loop-invariant
     for (auto &BI : L->getBlocks()) {
       for (Instruction &instr : *BI) {
         bool isInvariant = true;
-        if(instr.isBinaryOp() ) { 
-          outs()<<"Istruzione:"<<instr<<'\n';
+        if(instr.isBinaryOp()) { 
           for(auto *Iter=instr.op_begin(); Iter!=instr.op_end();++Iter) {
             Value *operand=*Iter;
-            outs()<<"Istruzione che definisce l'operando:"<<*operand<<'\n';
             check(operand, L, isInvariant, invariantMap);
           }
-          outs() << isInvariant <<"\n";
+          invariantMap[&instr] = isInvariant;
         }
       }
     }
 
     L->getExitBlocks(exitBlocks);
+    
+    // scegliere le instruzioni per la code motion
+    for(auto& invariantInst : invariantMap) {
+      if(invariantInst.second) {
+        BasicBlock* instBB = dyn_cast<Instruction>(invariantInst.first)->getParent();
+        bool isExitDominating = true;
+        bool isUsesDominating = true;
+
+        // controlliamo che domini tutte le uscite
+        for(auto& exitBlock : exitBlocks) {
+          isExitDominating &= DT->dominates(instBB, exitBlock);
+          if(!isExitDominating)
+            break;
+        }
+
+        // controlliamo che domini tutti gli usi
+        for(auto &use : invariantInst.first->uses()) {
+          BasicBlock* useExitBlock = dyn_cast<Instruction>(use)->getParent();
+          isUsesDominating &= DT->dominates(instBB, useExitBlock);
+          if(!isUsesDominating)
+            break;
+        }
+
+        if(isExitDominating && isUsesDominating)
+          CMcandidates.push_back(invariantInst.first);
+      }
+    }
+
+    // itero sul dominator tree in DFS
+    for(auto node = GraphTraits<DominatorTree *>::nodes_begin(DT); node != GraphTraits<DominatorTree *>::nodes_end(DT); ++node) {
+      BasicBlock *BB = node->getBlock();
+      for(Instruction &instr : *BB) {
+        // controllo se l'istruzione corrente è candidata alla code motion
+        if(std::find(CMcandidates.begin(), CMcandidates.end(), &instr) != CMcandidates.end()) {
+          // la aggiungo ad un array ordinato
+          removable.push_back(&instr);
+        }
+      }
+    }
+
+    for(auto& inst : removable) {
+      outs() <<*inst<<"\n";
+      inst->removeFromParent();
+      inst->insertBefore(preheader->getTerminator());
+    }
 
     return false;
   }
@@ -52,12 +100,11 @@ class LoopWalkPass final : public LoopPass {
     Instruction* reachingDef = dyn_cast<Instruction>(operand);
     if(reachingDef) {
       BasicBlock* bb = reachingDef->getParent();
-      if(L->contains(bb) && !invariantMap[reachingDef] )
+      if(L->contains(bb) && !invariantMap[reachingDef])
         isInvariant = false;
     }
   }
 };
-
 
 
 char LoopWalkPass::ID = 0;
