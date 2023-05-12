ee6cc9f404b4bf7126732328738244255756a51a
Luca Montanari <43385436+WhynotMonta@users.noreply.github.com>
2023-05-12 17:57:36 +0200

    Commenti per capire Transform.cpp

--- Loop_Fusion/lib/Transform.cpp
+++ Loop_Fusion/lib/Transform.cpp
@@ -46,6 +46,8 @@ void loopFusion(Loop *prevLoop, Loop *currentLoop, ScalarEvolution &SE) {
   replaceInductionVariables(prevLoop, currentLoop, SE);
 
   // body del primo loop va nel body del secondo loop
+  // vado a prendere l'ultima istruzione dell'ultimo blocco del 1* loop
+  // e poi prendo il primo basick block del 2* loop, cioè il loopp interno quando la condizione del loop è true (getSuccessor(0))
   BasicBlock *firstBodyBlock = prevLoop->getLoopLatch()->getPrevNode();
   Instruction *firstLoopBodyBranch = firstBodyBlock->getTerminator();
 
@@ -53,20 +55,24 @@ void loopFusion(Loop *prevLoop, Loop *currentLoop, ScalarEvolution &SE) {
   BasicBlock *secondLoopBodyBlock = secondLoopHeaderBranchInst->getSuccessor(0);
 
   // body del secondo loop va nel latch del primo loop
+  // selezione il blocco latch del primo loop 
+  // selezione l'ultima istruzione del primo blocco interno del 2* loop
   BasicBlock *firstLoopLatchBlock = prevLoop->getLoopLatch();
   Instruction *secondLoopBodyBranchInst = secondLoopBodyBlock->getTerminator();
 
   // in caso di loop end, l'header del primo blocco va nell'exit del secondo loop
+  // prendo l'exit block del 2* loop e prendo la condizione dell'header del 1* loop
   BasicBlock *secondLoopExitBlock = currentLoop->getExitBlock();
   Instruction *firstLoopHeaderBranchInst = prevLoop->getHeader()->getTerminator();
 
   //rimuovo latch del secondo loop
+  // prendo il blocco latch del 2* loop
   BasicBlock *secondLoopLatchBlock = currentLoop->getLoopLatch();
 
   // tutte le operazioni devono essere fatte alla fine
-  firstLoopBodyBranch->setSuccessor(0, secondLoopBodyBlock);
-  firstLoopHeaderBranchInst->setSuccessor(1, secondLoopExitBlock);
-  secondLoopBodyBranchInst->setSuccessor(0,firstLoopLatchBlock);
+  firstLoopBodyBranch->setSuccessor(0, secondLoopBodyBlock);                      // metto la prima istr. del secondo loop dopo l'ultima istr. di branch del primo loop
+  firstLoopHeaderBranchInst->setSuccessor(1, secondLoopExitBlock);                // setto che in caso di condizione falsa del 1* loop esco nell'exit block del 2* loop 
+  secondLoopBodyBranchInst->setSuccessor(0,firstLoopLatchBlock);                  // setto che dopo l'ultima istruzione di branch del primo blocco nel 2* loop, in caso sia true si vada al latch del 1* loop
 
   // facoltativo, eliminare il latch del secondo loop
   secondLoopLatchBlock->replaceAllUsesWith(currentLoop->getHeader());
